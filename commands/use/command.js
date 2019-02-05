c4cb474b700925dc33db15fae4d14021f64ae684
Zeboto <zebrawii@gmail.com>
2019-02-05 17:37:58 -0500

    Use command base

---
+++ commands/use/command.js
@@ -0,0 +1,30 @@
+const CommandBaseClass = require('../CommandBaseClass.js');
+
+class Use extends CommandBaseClass {
+    constructor(...args) {
+        super(...args);
+
+        this.meta = {
+            name: 'use',
+            category: 'meta.help.categories.battleblobs',
+            description: 'meta.help.commands.use',
+            aliases: ['useitem']
+        };
+    }
+
+    async run(context) {
+        const { message, client, connection } = context;
+
+        context.log('silly', 'acquiring user data for search..');
+        const userData = await connection.memberData(context.member);
+        const _ = (...x) => client.localize(userData.locale, ...x);
+        const _r = (...x) => client.localizeRandom(userData.locale, ...x);
+        context.log('silly', 'got user data');
+
+        if (userData.state_engaged) {
+            await context.send('You cannot do that right now.');
+            return;
+        }
+    }
+}
+module.exports = Use;
